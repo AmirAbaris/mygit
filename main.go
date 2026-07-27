a6509ff31f0368797a8fbf5f345ed4de7c9fd1b3
Abaris <thisisamirabaris@gmail.com>
2026-07-27 22:47:06 +0330

    Enhance main function to handle 'init' command and clarify os.Args usage

--- main.go
+++ main.go
@@ -6,5 +6,12 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Args)
+	// os Args: what did the user type after the program name?
+	args := os.Args
+
+	command := args[1]
+
+	if command == "init" {
+		fmt.Println("hit")
+	}
 }
